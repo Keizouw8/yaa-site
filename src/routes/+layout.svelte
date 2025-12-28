05779960305b12c6637cce74a8279c37b114359b
Keizou Wang <keizouw8@gmail.com>
2025-12-28 11:31:15 -0800

    mobile site wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
+<svelte:window bind:innerHeight bind:innerWidth></svelte:window>
 <svelte:head>
 	<link rel="icon" href="logo.png" />
 	<title>Young Author's Anthology</title>
 </svelte:head>
-{@render children?.()}
+{#if aspectRatio >= 1 }
+	{@render children?.()}
+{:else}
+	<center class="centered">
+		<h1>Mobile Site In-Progress</h1>
+		<p>Please visit the site on a desktop</p>
+	</center>
+{/if}
 <script lang="ts">
 	let { children } = $props();
 	import "$lib/global.css";
+
+	let innerHeight = $state(0);
+	let innerWidth = $state(0);
+	let aspectRatio = $derived(innerWidth / innerHeight);
 </script>
+<style>
+	.centered{
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
--- src/routes/landing.svelte
+++ src/routes/landing.svelte
@@ -1,7 +1,7 @@
 <div class="info">
 	<h1 class="title bold">Young Author's Anthology</h1>
-	<p class="subtitle">Quick 1-2 punch tagline explanation lorem ipsum dolor sit amet.</p>
-	<p style="margin-left: 25px;">Teacher? Click here.</p>
+	<p class="subtitle">A project for Washoe County Students by Washoe County students</p>
+	<!-- <p style="margin-left: 25px;">Teacher? Click here.</p> -->
 </div>
 <div class="fire" bind:this={fireContainer}>
 		<div class="flame" id="flame1"></div>
