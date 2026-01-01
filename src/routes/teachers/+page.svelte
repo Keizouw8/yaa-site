92358aed2abd49409279c2613fc615abc06ec0aa
Keizou Wang <keizouw8@gmail.com>
2025-12-31 23:01:25 -0800

    teachers page

--- src/routes/teachers/+page.svelte
+++ src/routes/teachers/+page.svelte
@@ -1,25 +1,44 @@
-<div class="left">
-	<h1>For teachers</h1>
-	<iframe id="video" src="https://www.youtube.com/embed/_vh-0EBqgR0?si=UxmilJ0ZLqMCQWpt&amp&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+<div class="container">
+	<div class="left">
+		<h1>For teachers</h1>
+		<iframe id="video" src="https://www.youtube.com/embed/{primaryVideo}?si=UxmilJ0ZLqMCQWpt&amp" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+	</div>
+	<div class="right">
+			<p>Students should check out the Young Author’s Anthology books in their school libraries. Ask parents for permission and teachers if you need any help with submitting through our Google Form. If you are selected as a winner, we would kindly ask for a photo of all authors so it can be placed into the anthology. We want people to get to know more about you through the photo and blurb even years after you graduate from your elementary school.</p>
+			<b>
+				<p>Submission Requirements</p>
+				<ol>
+					<li>Under 2000 words</li>
+					<li>Original and fictional works</li>
+					<li>School Appropriate</li>
+				</ol>
+			</b>
+	</div>
 </div>
-<div class="right">
-	<p>Content Ishan's going to give me</p>
+<h2>More Learning Materials</h2>
+<div class="carousel">
+	{#each videos as video}
+		<iframe src="https://www.youtube.com/embed/{video}?si=UxmilJ0ZLqMCQWpt&amp" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+	{/each}
 </div>
-<script lang="ts"></script>
+<script lang="ts">
+	let primaryVideo = "_vh-0EBqgR0";
+	let videos: string[] = ["qjiCTVFLrc0", "3vbJbPZpiTs", "hm3mJqJbLEo", "YbFAYk6y384", "ka6WQ-mwFbE", "CMgHRJiJnCw"];
+</script>
 <style>
-	div.left{
-		position: absolute;
-		left: 5vw;
-		top: 50%;
-		transform: translateY(-50%);
+	:global(body){
+		padding: 5vw;
+	}
+
+	div.container{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
 	}
 
 	div.right{
-		position: absolute;
-		right: 5vw;
 		width: 43.5vw;
-		top: 50%;
-		transform: translateY(-50%);
 	}
 
 	#video{
@@ -27,5 +46,20 @@
 		max-width: 43.5vw;
 		width: 100vw;
 		aspect-ratio: 16/9;
+	}
+
+	div.carousel{
+		position: relative;
+		width: 95vw;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		overflow-x: scroll;
+	}
+
+	div.carousel iframe{
+		height: 250px;
+		aspect-ratio: 16/9;
+		margin-right: 20px;
 	}
 </style>
