a70697ed013258bc5df2e8c12e19509666a0bdba
Keizou Wang <keizouw8@gmail.com>
2025-12-31 17:12:38 -0800

    video in teacher page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,30 +7,10 @@
 	<br>
 	Thirteen stories will be selected from the submission pool to be printed in a physical and digital anthology that will be sent back to the libraries of the winners and the schools listed below! All 4th and 5th graders are allowed to submit and schools interested in joining the partnership can refer to our Contact Us page for more information.</p>
 </div>
-<embed src="flyers/spring.pdf" type="application/pdf" id="pdfembed">
-<button id="reveal" onclick={() => videoActive = true}>Show Video</button>
-{#if videoActive}
-	<div id="shadow" onclick={() => videoActive = false}></div>
-	<iframe id="video" src="https://www.youtube.com/embed/_vh-0EBqgR0?si=UxmilJ0ZLqMCQWpt&amp&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
-{/if}
+<embed src="flyers/combined.pdf" type="application/pdf" id="pdfembed">
 <script lang="ts">
-	let videoActive = $state(true);
 </script>
 <style>
-	#reveal{
-		position: absolute;
-		bottom: 5vw;
-		right: 5vw;
-		border: none;
-		outline: none;
-		background: var(--black);
-		color: var(--white);
-		padding: 12px;
-		font-size: 18px;
-		border-radius: 5px;
-		cursor: pointer;
-	}
-
 	@keyframes blurin{
 		from{
 			background-color: rgba(0, 0, 0, 0);
@@ -40,24 +20,6 @@
 			background-color: rgba(0, 0, 0, 0.25);
 			backdrop-filter: blur(5px);
 		}
-	}
-
-	#shadow{
-		top: 0;
-		left: 0;
-		position: absolute;
-		height: 100%;
-		width: 100%;
-		animation: blurin forwards 0.25s;
-	}
-
-	#video{
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		height: 80%;
-		width: 80%;
-		transform: translate(-50%, -50%);
 	}
 
 	#pdfembed{
--- src/routes/teachers/+page.svelte
+++ src/routes/teachers/+page.svelte
@@ -1,2 +1,31 @@
-<h1>For teachers</h1>
-<p>in development</p>
+<div class="left">
+	<h1>For teachers</h1>
+	<iframe id="video" src="https://www.youtube.com/embed/_vh-0EBqgR0?si=UxmilJ0ZLqMCQWpt&amp&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+</div>
+<div class="right">
+	<p>Content Ishan's going to give me</p>
+</div>
+<script lang="ts"></script>
+<style>
+	div.left{
+		position: absolute;
+		left: 5vw;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+
+	div.right{
+		position: absolute;
+		right: 5vw;
+		width: 43.5vw;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+
+	#video{
+		max-height: 80vh;
+		max-width: 43.5vw;
+		width: 100vw;
+		aspect-ratio: 16/9;
+	}
+</style>
